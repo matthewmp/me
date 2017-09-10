a7d559707a3b8272790962a97acbd3c7c9ba7b91
Matt Palumbo <matt.m.palumbo@gmail.com>
2017-09-10 07:46:30 -0400

    changed backgrounds of projects, animated hamburger menu, changed portfolio images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-$( ".hamburger" ).click(function() {
-  $(".mobile-menu").slideToggle( "slow", function() {
-    // Animation complete.
-  });
-});
 
-$('a.scrollTo').click(function(){
-	let scrollTo = $(this).attr('data-scrollTo');
-	$('body, html').animate({
-		"scrollTop": $('.' + scrollTo).offset().top
-	}, 1000)
-})
+$(document).ready(function(){
+	$( '.hamburger' ).click(function() {
+		$('.hamburger').toggleClass('active', 5000);
+
+	  	$('.mobile-menu').slideToggle( 'slow', function() {
+	    	// Animation complete.
+	  	});
+
+
+	});
+
+	$('a.scrollTo').click(function(){
+		let scrollTo = $(this).attr('data-scrollTo');
+		$('body, html').animate({
+			'scrollTop': $('.' + scrollTo).offset().top
+		}, 1000)
+	});
+});
