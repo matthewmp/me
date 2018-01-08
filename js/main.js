a2c872f9555d126c7f232b0683e9bacbb389f623
Matt Palumbo <matt.m.palumbo@gmail.com>
2018-01-08 12:22:12 -0500

    tweaked main.js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 
 	$('#spinner').fadeOut();
 
-	$('.spHomeBtn').click()=>{
+	$('.spHomeBtn').click(function(){
 		let url = 'http://mattpalumbo.me';
 		$(location).attr('href',url);
 	});
